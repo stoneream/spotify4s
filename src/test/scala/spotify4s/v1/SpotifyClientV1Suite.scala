153e820a948a62ef3245da4057aae33f3eabb0ac
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-11-20 20:37:20 +0900

    トラック情報取ってみるテスト

--- src/test/scala/spotify4s/v1/SpotifyClientV1Suite.scala
+++ src/test/scala/spotify4s/v1/SpotifyClientV1Suite.scala
@@ -4,28 +4,35 @@ import akka.actor.ActorSystem
 import akka.stream.SystemMaterializer
 import org.scalatest.funsuite.AnyFunSuite
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
-import spotify4s.SpotifyClient
-import spotify4s.SpotifyClientV1.SpotifyClientV1
-import util.SpotifyClientTestSuiteMixin
+import spotify4s.v1.SpotifyClientV1.SpotifyClientOpt
+import spotify4s.{SpotifyClient, SpotifyOAuth2Client}
 
 import scala.concurrent.duration.DurationInt
 
-class SpotifyClientV1Suite extends AnyFunSuite with SpotifyClientTestSuiteMixin {
-  val accessToken = "accessTokenHere"
+class SpotifyClientV1Suite extends AnyFunSuite {
   val timeout = 30.seconds
 
-  test("/me") {
+  test("/track") {
     implicit val system = ActorSystem()
     implicit val materializer = SystemMaterializer(system).materializer
 
     val ws = StandaloneAhcWSClient()
+
+    val oauthClient = new SpotifyOAuth2Client("client-id-here", "client-secret-here")(ws, timeout)
+
+    // client credential flow
+    val accessTokenRequestResponse = oauthClient.accessTokenRequest
+    val accessToken = (accessTokenRequestResponse \ "access_token").asOpt[String].getOrElse(fail())
+
     val client = new SpotifyClient(accessToken)(ws, timeout)
-    val response = client.me
+    val track = client.track("3doNZXJKsQpIj3RaatDwpS")
 
-    println(response)
+    val trackName = (track \ "name").asOpt[String].getOrElse(fail)
+    val artistNames = (track \ "artists" \\ "name").flatMap(_.asOpt[String])
 
+    assert(trackName == "エイリアンズ - 2018 Remaster")
+    assert(artistNames.contains("KIRINJI"))
     system.terminate()
-
   }
 
 }
